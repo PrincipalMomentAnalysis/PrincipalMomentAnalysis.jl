59b203458c968a283a370f5bf4a88873be717f26
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2019-11-09 16:54:28 +0100

    Added export of loadings

--- gui/pmagui.jl
+++ gui/pmagui.jl
@@ -13,6 +13,8 @@ using Colors
 using PlotlyJS
 using Measures
 
+using DelimitedFiles
+
 include("qlucore.jl")
 include("pmaplots.jl")
 include("pmaplots_gadfly.jl")
@@ -41,21 +43,24 @@ function opendataset(filepath::String)::Dataset
 end
 
 
-struct DimReduction
+struct Result
 	U::Matrix{Float64}
 	S::Vector{Float64}
 	V::Matrix{Float64}
+	G::AbstractMatrix # the graph (needed for plotting)
 end
-DimReduction() = DimReduction(zeros(0,0),zeros(0),zeros(0,0))
+Result() = Result(zeros(0,0),zeros(0),zeros(0,0),zeros(0,0))
 
 
 function populategui!(w, ds::Dataset)
 	infoStr = ""
 	sa = String[]
+	va = String[]
 
 	if isempty(ds.errorMsg)
 		infoStr = """File: "$(ds.filepath)" with $(size(ds.data,2)) samples, $(size(ds.data,1)) variables, $(size(ds.sa,2)) sample annotations and $(size(ds.va,2)) variable annotations."""
 		sa = string.(names(ds.sa))
+		va = string.(names(ds.va))
 	else
 		infoStr = ds.errorMsg
 	end
@@ -80,7 +85,15 @@ function populategui!(w, ds::Dataset)
 			       document.getElementById("timeAnnot").options.add(opt);""")
 	end
 
-	# js(w, js"""console.log('$infoStr');""")	
+	# TODO: simple implementation, optimize
+	js(w, js"""document.getElementById("exportVarAnnot").options.length = 0;""")
+	for a in va
+		js(w, js"""var opt = document.createElement("option");
+			       opt.value = $a;
+			       opt.text = $a;
+			       document.getElementById("exportVarAnnot").options.add(opt);""")
+	end
+
 end
 
 
@@ -96,11 +109,13 @@ function changesamplemethod!(w, sampleMethod::Symbol)
 	js(w, js"""document.getElementById("distNearestNeighbors").disabled = $(!enabled);""")
 end
 
-function rundimreduction(ds::Dataset, dimReductionMethod::Symbol, sampleMethod::Symbol, sampleAnnotation::Symbol, timeAnnotation::Symbol, kNearestNeighbors::Int, distNearestNeighbors::Float64, plotDims::Int)
+function rundimreduction(ds::Dataset;
+                         dimReductionMethod::Symbol, sampleMethod::Symbol, sampleAnnotation::Symbol,
+                         timeAnnotation::Symbol, kNearestNeighbors::Int, distNearestNeighbors::Float64)
 	isempty(ds.errorMsg) || return
 	@assert dimReductionMethod in (:PMA,:PCA)
 	@assert sampleMethod in (:SA,:Time,:NN,:NNSA)
-	@assert plotDims in 2:3
+	# @assert plotDims in 2:3
 
 	X = zeros(size(ds.data))
 	if any(ismissing,ds.data)
@@ -129,15 +144,20 @@ function rundimreduction(ds::Dataset, dimReductionMethod::Symbol, sampleMethod::
 		G = neighborhoodgraph(X,kNearestNeighbors,distNearestNeighbors,50; groupBy=ds.sa[!,sampleAnnotation]);
 	end
 
-	dim = plotDims
+	# dim = 3
+	dim = min(10, size(X)...)
 
 	if dimReductionMethod==:PMA
 		U,S,V = pma(X,G,dim=dim)
 	elseif dimReductionMethod==:PCA
 		U,S,V = pca(X,dim=dim)
 	end
-	F = DimReduction(U,S,V)
+	Result(U,S,V,G)
+end
 
+function showplot(ds::Dataset, result::Result, plotDims::Int;
+                  dimReductionMethod::Symbol, sampleAnnotation::Symbol,
+                  kwargs...)
 	colorBy = sampleAnnotation
 	colorDict = colordict(ds.sa[!,colorBy])
 
@@ -153,7 +173,7 @@ function rundimreduction(ds::Dataset, dimReductionMethod::Symbol, sampleMethod::
 		markerSize = 5
 		lineWidth = 1
 
-		pl = plotsimplices(F.V,ds.sa,G,colorBy,colorDict, title=title,
+		pl = plotsimplices(result.V,ds.sa,result.G,colorBy,colorDict, title=title,
 		                   drawTriangles=drawTriangles, drawLines=drawLines, drawPoints=true,
 		                   opacity=opacity, markerSize=markerSize, lineWidth=lineWidth,
 		                   width=1024, height=768)
@@ -162,7 +182,7 @@ function rundimreduction(ds::Dataset, dimReductionMethod::Symbol, sampleMethod::
 		markerSize = 0.9mm
 		lineWidth = 0.3mm
 
-		pl = plotsimplices_gadfly(F.V,ds.sa,G,colorBy,colorDict, title=title,
+		pl = plotsimplices_gadfly(result.V,ds.sa,result.G,colorBy,colorDict, title=title,
 		                          drawTriangles=drawTriangles, drawLines=drawLines, drawPoints=true,
 		                          opacity=opacity, markerSize=markerSize, lineWidth=lineWidth)
 		display(pl)
@@ -170,15 +190,49 @@ function rundimreduction(ds::Dataset, dimReductionMethod::Symbol, sampleMethod::
 end
 
 
+function exportloadings(ds::Dataset, result::Result, filepath::String, varAnnot::Symbol, columns::Symbol, dims::Int, columnSort::Symbol;
+                        dimReductionMethod::Symbol, sampleAnnotation::Symbol,
+                        kwargs...)
+	@assert columns in (:All, :Selected)
+	@assert columnSort in (:Abs, :Descending)
+
+	df = DataFrame()
+	df[!,varAnnot] = ds.va[!,varAnnot]
+
+	if columns==:All
+		for i=1:min(dims,length(result.S))
+			colName = Symbol("$dimReductionMethod$i")
+			df[!,colName] = result.U[:,i]
+		end
+	elseif columns==:Selected
+		colName = Symbol("$dimReductionMethod$dims")
+		df[!,colName] = result.U[:,dims]
+
+		if columnSort==:Abs
+			sort!(df, colName, by=abs, rev=true)
+		elseif columnSort==:Descending
+			sort!(df, colName, rev=true)
+		end
+	end
+
+	# TODO: add CSV dependency and save using CSV.write() instead?
+	mat = Matrix{String}(undef, size(df,1)+1, size(df,2))
+	mat[1,:] .= string.(names(df))
+	mat[2:end,:] .= string.(df)
+	writedlm(filepath, mat, '\t')
+end
+
 
 function main()
 	# init
 	dataset = opendataset("")
+	dimReductionParams = nothing
+	result = nothing
 	messageQueue = Queue{Pair{String,Any}}()
 
 
 	# setup gui
-	w = Window(Dict(:width=>512,:height=>512))
+	w = Window(Dict(:width=>512,:height=>768))
 
 	# event listeners
 	handle(w, "gedataopen") do args
@@ -194,6 +248,10 @@ function main()
 		println("showplot: ", args)
 		enqueue!(messageQueue, "showplot"=>args)
 	end
+	handle(w, "exportloadings") do args
+		println("exportloadings: ", args)
+		enqueue!(messageQueue, "exportloadings"=>args)
+	end
 
 	doc = read(joinpath(@__DIR__,"content.html"),String)
 	body!(w,doc,async=false)
@@ -208,6 +266,8 @@ function main()
 				filepath = msg.second
 				println("Loading ", filepath)
 				dataset = opendataset(filepath)
+				dimReductionParams = nothing
+				result = nothing
 				if isempty(dataset.errorMsg)
 					println(filepath, " loaded successfully")
 				else
@@ -219,20 +279,87 @@ function main()
 				println("Changing method to ", sampleMethod)
 				changesamplemethod!(w,sampleMethod)
 			elseif msg.first == "showplot"
-				println("Showing plot")
 				args = msg.second
 				dimReductionMethod = Symbol(args[1])
+
+				params = Dict()
 				try
-					isempty(dataset.errorMsg) || error(dataset.errorMsg)
-					sampleMethod = Symbol(args[2])
-					sampleAnnotation = Symbol(args[3])
-					timeAnnotation = Symbol(args[4])
-					kNearestNeighbors = parse(Int,args[5])
-					distNearestNeighbors = parse(Float64,args[6])
-					plotDims = parse(Int,args[7])
-					rundimreduction(dataset,dimReductionMethod,sampleMethod, sampleAnnotation, timeAnnotation, kNearestNeighbors, distNearestNeighbors, plotDims)
+					params = Dict(:dimReductionMethod=>dimReductionMethod,
+					              :sampleMethod=>Symbol(args[2]),
+					              :sampleAnnotation=>Symbol(args[3]),
+					              :timeAnnotation=>Symbol(args[4]),
+					              :kNearestNeighbors=>parse(Int,args[5]),
+					              :distNearestNeighbors=>parse(Float64,args[6]))
 				catch e
-					println("Failed to run ", dimReductionMethod,": ", sprint(showerror, e))
+					println("Failed to parse parameters ", args, ": ", sprint(showerror, e))
+				end
+
+				if params != dimReductionParams
+					try
+						println("Running ", dimReductionMethod)
+						result = rundimreduction(dataset; params...)
+						println("Done")
+						dimReductionParams = params
+					catch e
+						println("Failed to run ", dimReductionMethod, ": ", sprint(showerror, e))
+						result = nothing
+						dimReductionParams = nothing
+					end
+				end
+
+				if result!=nothing
+					try
+						println("Showing plot")
+						plotDims = parse(Int,args[7])
+						showplot(dataset, result, plotDims; dimReductionParams...)
+					catch e
+						println("Error showing plot: ", sprint(showerror,e))
+					end
+				end
+			elseif msg.first == "exportloadings"
+				# TODO: avoid code duplication
+				args = msg.second
+				dimReductionMethod = Symbol(args[1])
+
+				params = Dict()
+				try
+					params = Dict(:dimReductionMethod=>dimReductionMethod,
+					              :sampleMethod=>Symbol(args[2]),
+					              :sampleAnnotation=>Symbol(args[3]),
+					              :timeAnnotation=>Symbol(args[4]),
+					              :kNearestNeighbors=>parse(Int,args[5]),
+					              :distNearestNeighbors=>parse(Float64,args[6]))
+				catch e
+					println("Failed to parse parameters ", args, ": ", sprint(showerror, e))
+				end
+
+				if params != dimReductionParams
+					try
+						println("Running ", dimReductionMethod)
+						result = rundimreduction(dataset; params...)
+						println("Done")
+						dimReductionParams = params
+					catch e
+						println("Failed to run ", dimReductionMethod, ": ", sprint(showerror, e))
+						result = nothing
+						dimReductionParams = nothing
+					end
+				end
+
+				if result!=nothing
+					try
+						println("Exporting loadings")
+						println(args)
+						filepath = args[7]
+						varAnnot = Symbol(args[8])
+						columns = Symbol(args[9])
+						dims = parse(Int,args[10])
+						columnSort = Symbol(args[11])
+						exportloadings(dataset, result, filepath, varAnnot, columns, dims, columnSort; dimReductionParams...)
+						println("Done")
+					catch e
+						println("Error exporting loadings: ", sprint(showerror,e))
+					end
 				end
 			else
 				@warn "Unknown message type: $(msg.first)"
