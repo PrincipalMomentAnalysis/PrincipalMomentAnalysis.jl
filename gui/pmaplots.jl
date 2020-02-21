04ffbc939f4681bb02c0ba3749f8c8861a1bd06e
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-02-21 21:51:17 +0100

    Improved line plotting

--- gui/pmaplots.jl
+++ gui/pmaplots.jl
@@ -8,8 +8,6 @@ function plotsimplices(V, sa, G, colorBy, colorDict;
 
 	# plot each group in different colors
 	if drawPoints
-
-
 		# testing plotting with colorscale instead of dict when colorDict=nothing.
 		# TODO: merge the two cases below and implement for lines/triangles too
 
@@ -39,19 +37,22 @@ function plotsimplices(V, sa, G, colorBy, colorDict;
 	end
 
 	if drawLines
-		# plot lines based on graph
+		x = Union{Nothing,Float64}[]
+		y = Union{Nothing,Float64}[]
+		z = Union{Nothing,Float64}[]
+		colors = RGB{Float64}[]
 		for ci in findall(G)
 			r,c = Tuple(ci)
 			r>c || continue # just use lower triangular part
-			col = weighted_color_mean(0.5, colorDict[sa[r,colorBy]], colorDict[sa[c,colorBy]]) # mix the colors... is it possible to do a gradient?
-			ind = [r,c]
-			line = scatter3d(;x=V[ind,1],y=V[ind,2],z=V[ind,3], mode="lines", line=attr(color=col, width=lineWidth), showlegend=false)
-			push!(traces, line)
+			push!(x, V[r,1], V[c,1], nothing)
+			push!(y, V[r,2], V[c,2], nothing)
+			push!(z, V[r,3], V[c,3], nothing)
+			push!(colors, colorDict[sa[r,colorBy]], colorDict[sa[c,colorBy]], RGB(0.,0.,0.))
 		end
+		push!(traces, scatter3d(;x=x,y=y,z=z, mode="lines", line=attr(color=colors, width=lineWidth), showlegend=false))
 	end
 
 	if drawTriangles
-		# plot triangles based on graph - TODO: improve code!
 		triangleInds = Int[]
 		for c=1:size(G,1)
 			ind = findall(G[:,c])
